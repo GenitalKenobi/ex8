07d8aee9256144602359505797f90f62579914bb
GenitalKenobi <prashantss321@gmail.com>
2023-11-12 21:59:32 +0530

    second commit

--- monitoring.py
+++ monitoring.py
@@ -1,29 +1,26 @@
 import mlflow
 import mlflow.sklearn
-from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
 from sklearn.datasets import load_breast_cancer
-from sklearn.svm import NuSVC
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 df = load_breast_cancer()
 X = df.data
 y = df.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
 with mlflow.start_run(nested=True):
-    model = NuSVC()
-    mse_list = []
+    model = MLPRegressor()
 
-    mlflow.log_params(model.get_params())
+    mlflow.log_params(
+        model.get_params()
+    )
 
-    for epoch in range(20):
-        model.fit(X_train, y_train)
+    for step in range(50):
+        model.partial_fit(X_train, y_train)
         y_pred = model.predict(X_test)
-        mse = mean_squared_error(y_pred, y_test)
-        mse_list.append(mse)
-
-        mlflow.log_metric("epoch mse", mse, step=epoch)
-
+        accuracy = mean_squared_error(y_pred, y_test)
+        mlflow.log_metric('epoch accuracy', accuracy, step=step)
     mlflow.sklearn.save_model(model, 'model')
-
 mlflow.end_run()
